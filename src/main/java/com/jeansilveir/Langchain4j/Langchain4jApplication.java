dec1b52a2c630c37c5bdc17f50c9948c649a23e1
Jean Silveira <silveirajeans@gmail.com>
2025-10-27 20:18:51 -0300

    start app with LLM

---
+++ src/main/java/com/jeansilveir/Langchain4j/Langchain4jApplication.java
@@ -0,0 +1,13 @@
+package com.jeansilveir.Langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(Langchain4jApplication.class, args);
+	}
+
+}
